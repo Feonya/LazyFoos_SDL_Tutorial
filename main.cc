994850feec72bf64cad9124ec0793eb0f7a62ce1
tuxyin <wangyinyin@outlook.com>
2019-11-27 15:10:30 +0800

    Timing.

--- main.cc
+++ main.cc
@@ -1,94 +1,88 @@
-/// Copyright 2019 Tuxyin.
-
 #include <string>
+#include <sstream>
 
 #include "SDL2/SDL.h"
-#include "SDL2/SDL_image.h"
-#include "SDL2/SDL_mixer.h"
+#include "SDL2/SDL_ttf.h"
 
-SDL_Window*   window   = NULL;
-SDL_Renderer* renderer = NULL;
-SDL_Texture*  texture  = NULL;
-
-// The music that will be played.
-Mix_Music* music   = NULL;
-// The sound effects that will be used.
-Mix_Chunk* kiss    = NULL;
-Mix_Chunk* dog     = NULL;
-Mix_Chunk* coin    = NULL;
-Mix_Chunk* oh_yeah = NULL;
+SDL_Window*   window         = NULL;
+SDL_Renderer* renderer       = NULL;
+SDL_Texture*  prompt_texture = NULL;
+SDL_Texture*  text_texture   = NULL;
+TTF_Font*     font           = NULL;
 
 bool init();
-bool loadMedia();
+bool load_media();
 void close();
+SDL_Texture* create_texture_from_text(std::string text, SDL_Color color);
 
-int main(int argc, char* argv[]) {
+int main(int argc, char* argv[])
+{
   bool quit = false;
 
-  if (init() == false) {
-    printf("Initialize SDL unsuccessful!\n");
-    quit = true;
-  }
-
-  if (loadMedia() == false) {
-    printf("Load media unsuccessfull!\n");
-    quit = true;
-  }
+  if (init() == false)       quit = true;
+  if (load_media() == false) quit = true;
 
   SDL_Event e;
 
-  while (!quit) {
-    while (SDL_PollEvent(&e)) {
-      if (e.type == SDL_QUIT) {
-        quit = true;
-      // Handle key press.
-      } else if (e.type == SDL_KEYDOWN) {
-        switch (e.key.keysym.sym) {
-          // Play sound effects.
-          case SDLK_1:
-            Mix_PlayChannel(-1, kiss, 0);
-            break;
-          case SDLK_2:
-            Mix_PlayChannel(-1, dog, 0);
-            break;
-          case SDLK_3:
-            Mix_PlayChannel(-1, coin, 0);
-            break;
-          case SDLK_4:
-            Mix_PlayChannel(-1, oh_yeah, 0);
-            break;
+  // Current time start time.
+  Uint32 start_time = 0;
+  // In memory text stream.
+  std::stringstream time_text;
+  // Text color.
+  SDL_Color color = {0, 0, 0, 255};
 
-          // Start, pause or resume play music.
-          case SDLK_9:
-            // If there is no music playing.
-            if (Mix_PlayingMusic() == 0) {
-              // Play the music.
-              Mix_PlayMusic(music, 0);
-            // If music is being paused.
-            } else {
-              // If the music is paused.
-              if (Mix_PausedMusic() == 1) {
-                // Resume the music.
-                Mix_ResumeMusic();
-              // If the music is playing.
-              } else {
-                // Pause the music.
-                Mix_PauseMusic();
-              }
-            }
-            break;
+  // Text rectangles.
+  SDL_Rect prompt_rect;
+  SDL_Rect text_rect;
+  // Text texture width and height.
+  int prompt_w = 0;
+  int prompt_h = 0;
+  int text_w   = 0;
+  int text_h   = 0;
 
-          // Stop music.
-          case SDLK_0:
-            Mix_HaltMusic();
-            break;
-        }
-      }
+  // Get prompt texture width and height.
+  SDL_QueryTexture(prompt_texture, NULL, NULL, &prompt_w, &prompt_h);
+  // Set prompt rect.
+  prompt_rect = {0, 0, prompt_w, prompt_h};
+
+  while (!quit)
+  {
+    while (SDL_PollEvent(&e))
+    {
+      if (e.type == SDL_QUIT) quit = true;
+
+      // Reset start time on return keypress.
+      else if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_RETURN)
+        start_time = SDL_GetTicks();
     }
 
-    // Render texture.
+    // Set text to be rendered.
+    time_text.str("");
+    time_text << "Milliseconds since start time "
+              << SDL_GetTicks() - start_time;
+
+    // Start Rendering text.
+    // Free text_texture's memory first.
+    SDL_DestroyTexture(text_texture);
+    // Then create texture from time text.
+    text_texture = create_texture_from_text(time_text.str(), color);
+    if (text_texture == NULL)
+    {
+      printf("Failed to create time text texture: %s\n", SDL_GetError());
+      quit = true;
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0xFF, 0xFF, 0xFF, 0xFF);
     SDL_RenderClear(renderer);
-    SDL_RenderCopy(renderer, texture, 0, 0);
+
+    SDL_RenderCopy(renderer, prompt_texture, 0, &prompt_rect);
+
+    // Get text texture width and height.
+    SDL_QueryTexture(text_texture, NULL, NULL, &text_w, &text_h);
+    // Set text rect.
+    text_rect = {0, 240, text_w, text_h};
+    SDL_RenderCopy(renderer, text_texture, 0, &text_rect);
+
     SDL_RenderPresent(renderer);
   }
 
@@ -96,9 +90,11 @@ int main(int argc, char* argv[]) {
   return 0;
 }
 
-bool init() {
-  // Initialize SDL subsystems.
-  if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0) {
+bool init()
+{
+  // Initialize SDL subsystem.
+  if (SDL_Init(SDL_INIT_VIDEO) != 0)
+  {
     printf("Failed to initialize SDL: %s\n", SDL_GetError());
     return false;
   }
@@ -107,109 +103,77 @@ bool init() {
   window = SDL_CreateWindow("Lazy Foo's SDL Tutorial",
                             SDL_WINDOWPOS_UNDEFINED,
                             SDL_WINDOWPOS_UNDEFINED,
-                            800, 640,
-                            SDL_WINDOW_SHOWN);
-  if (window == NULL) {
-    printf("Faile to create SDL window: %s\n", SDL_GetError());
+                            640, 480, SDL_WINDOW_SHOWN);
+  if (window == NULL)
+  {
+    printf("Failed to create window: %s\n", SDL_GetError());
     return false;
   }
-
+  
   // Create SDL renderer.
   renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-  if (renderer == NULL) {
-    printf("Failed to create SDL renderer: %s\n", SDL_GetError());
+  if (renderer == NULL)
+  {
+    printf("Failed to create renderer: %s\n", SDL_GetError());
     return false;
   }
 
-  // Initialize PNG loading.
-  if ((IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG) != IMG_INIT_PNG) {
-    printf("Failed to initialize SDL image: %s\n", IMG_GetError());
+  // Initialize SDl ttf.
+  if (TTF_Init() == -1)
+  {
+    printf("Failed to initalize SDL ttf: %s\n", TTF_GetError());
     return false;
   }
 
-  // Initialize Audio loading.
-  if ((Mix_Init(MIX_INIT_MP3) & MIX_INIT_MP3) != MIX_INIT_MP3) {
-    printf("Failed to initialize SDL mix: %s\n", Mix_GetError());
-    return false;
-  }
-
-  // Initialize the mixer API.
-  if (Mix_OpenAudio(44100, AUDIO_S16SYS, 2, 1024) == -1) {
-    printf("Failed to open audio from SDL mix: %s\n", Mix_GetError());
-    return false;
-  }
-
-  // Everything is OK.
-  return  true;
-}
-
-bool loadMedia() {
-  // Load texture.
-  SDL_Surface* surf = IMG_Load("assets/bg.png");
-  texture = SDL_CreateTextureFromSurface(renderer, surf);
-  if (texture == NULL) {
-    printf("Failed to load texture: %s\n", SDL_GetError());
-    return false;
-  }
-  SDL_FreeSurface(surf);
-
-  // Load music.
-  music = Mix_LoadMUS("assets/music.mp3");
-  if (music == NULL) {
-    printf("Failed to load music: %s\n", Mix_GetError());
-    return false;
-  }
-
-  // Load sound effects.
-  kiss = Mix_LoadWAV("assets/kiss.wav");
-  if (kiss == NULL) {
-    printf("Failed to load sound kiss: %s\n", Mix_GetError());
-    return false;
-  }
-
-  dog = Mix_LoadWAV("assets/dog.wav");
-  if (dog == NULL) {
-    printf("Failed to load sound dog: %s\n", Mix_GetError());
-    return false;
-  }
-
-  coin = Mix_LoadWAV("assets/coin.wav");
-  if (coin == NULL) {
-    printf("Failed to load sound coin: %s\n", Mix_GetError());
-    return false;
-  }
-
-  oh_yeah = Mix_LoadWAV("assets/oh_yeah.wav");
-  if (oh_yeah == NULL) {
-    printf("Failed to load sound oh_yeah: %s\n", Mix_GetError());
-    return false;
-  }
-
-  // Everything is OK.
   return true;
 }
 
-void close() {
-  SDL_DestroyTexture(texture);
-  texture = NULL;
+bool load_media()
+{
+  // Load the font.
+  font = TTF_OpenFont("comic.ttf", 28);
+  if (font == NULL)
+  {
+    printf("Failed to load comic font: %s\n", TTF_GetError());
+    return false;
+  }
 
-  Mix_FreeMusic(music);
-  Mix_FreeChunk(kiss);
-  Mix_FreeChunk(dog);
-  Mix_FreeChunk(coin);
-  Mix_FreeChunk(oh_yeah);
-  music   = NULL;
-  kiss    = NULL;
-  dog     = NULL;
-  coin    = NULL;
-  oh_yeah = NULL;
+  // create texture from given text.
+  SDL_Color color = {0, 0, 0, 255};
+  prompt_texture = create_texture_from_text("Press Enter to Reset Start Time.",
+                                            color);
+  if (prompt_texture == NULL)
+  {
+    printf("Failed to create prompt text texture: %s\n", SDL_GetError());
+    return false;
+  }
 
+  return true;
+}
+
+SDL_Texture* create_texture_from_text(std::string text, SDL_Color color)
+{
+  SDL_Surface* surf = TTF_RenderText_Solid(font, text.c_str(), color);
+  SDL_Texture* texture;
+  texture = SDL_CreateTextureFromSurface(renderer, surf);
+  SDL_FreeSurface(surf);
+
+  return texture;
+}
+
+void close()
+{
+  TTF_CloseFont(font);
+  SDL_DestroyTexture(prompt_texture);
+  SDL_DestroyTexture(text_texture);
   SDL_DestroyRenderer(renderer);
   SDL_DestroyWindow(window);
-  renderer = NULL;
-  window   = NULL;
-
-  Mix_Quit();
-  IMG_Quit();
+  font           = NULL;
+  prompt_texture = NULL;
+  text_texture   = NULL;
+  renderer       = NULL;
+  window         = NULL;
+  
+  TTF_Quit();
   SDL_Quit();
 }
